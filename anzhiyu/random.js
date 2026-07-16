310b7e9b1519d8c95ce991d907a6bf928c6e80a1
zhaoguohui09820 <zhaolixiangguohui@outlook.com>
2026-07-16 16:43:25 +0800

    Site updated: 2026-07-16 16:43:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["2024/07/18/“仙人”的画作/","2024/08/13/行走在云端，感受日落的美/","2024/07/29/加入我的Minecraft 服务器/"];function toRandomPost(){
+var posts=["2024/07/18/“仙人”的画作/","2024/07/29/加入我的Minecraft 服务器/","2024/08/13/行走在云端，感受日落的美/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[];
     var refreshNum = 1;
